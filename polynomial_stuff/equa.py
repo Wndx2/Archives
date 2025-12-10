8f045c4af2911c63dbf0708c1e420709a843322d
Wndx2 <168486501+Wndx2@users.noreply.github.com>
2025-12-11 12:55:21 +1300

    Update equa.py

--- polynomial_stuff/equa.py
+++ polynomial_stuff/equa.py
@@ -1,17 +1,22 @@
+# TODO: add ANSI colors here
+
 import os
 
+
 def clear():
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
+
+
 clear()
 
 print("\nPolynomial (Degree 2)\n")
 
 while True:
     try:
-        print("========================")
+        clear()
 
         a = float(input("\na : "))
         b = float(input("\nb : "))
@@ -21,21 +26,23 @@ while True:
             print("\nMa ERROR.\n")
             continue
 
-        d = b**2 - 4*a*c  # DISCRIMINANT (** = ^)
+        d = b**2 - 4 * a * c  # DISCRIMINANT (** = ^)
 
         if d > 0:
             # '**0.5' = '√', AS POWER TO 1/2 (0.5) IS SQUARE ROOTING.
-            x1 = (-b + d**0.5) / (2*a)
-            x2 = (-b - d**0.5) / (2*a)
-            print("\nTwo Real Solutions (D > 0):")
+            x1 = (-b + d**0.5) / (2 * a)
+            x2 = (-b - d**0.5) / (2 * a)
+            print(f"\nTwo Real Solutions, as D = {d} (D > 0):")
             print(f"    X1 [{x1}]")
             print(f"    X2 [{x2}]\n")
         elif d == 0:
-            x = -b / (2*a)
-            print("\nOne Real Solution (D = 0):")
+            x = -b / (2 * a)
+            print(f"\nOne Real Solution, as D = {d} (D = 0):")
             print(f"    X1 [{x}] * 2\n")
         else:
-            print("\nNo Real Solution(s) (D < 0).\n")
+            print(f"\nNo Real Solution(s), as D = {d} (D < 0).\n")
+
+        confirmation = input()
 
     except ValueError:
         print("Invalid Input.\n")
