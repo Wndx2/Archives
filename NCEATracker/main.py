9409652e672992521a70cd5399f771fc18800db8
Wndx2 <168486501+Wndx2@users.noreply.github.com>
2025-12-30 17:59:59 +1300

    asdf

--- NCEATracker/main.py
+++ NCEATracker/main.py
@@ -1,9 +1,26 @@
-import sqlite3 as sql
-import matplotlib.pyplot as plt
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+# from PySide6.QtCharts import QChart, QChartView, QPieSeries
 
-labels = ["A", "B", "C"]
-sizes = [40, 35, 25]
+app = QApplication(sys.argv)
 
-plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=90)
-plt.axis("equal")  # keeps it circular instead of a sad oval
-plt.show()
+"""
+series = QPieSeries()
+series.append("Math", 30)
+series.append("Science", 25)
+series.append("English", 45)
+
+chart = QChart()
+chart.addSeries(series)
+chart.setTitle("2025 Credits")
+
+chart_view = QChartView(chart)
+chart_view.setRenderHint(chart_view.renderHints())
+"""
+
+window = QMainWindow()
+# window.setCentralWidget(chart_view) --> gonna be used later on
+window.resize(1900, 1600)  # fullscreen on macbook air m1
+window.show()
+
+app.exec()
